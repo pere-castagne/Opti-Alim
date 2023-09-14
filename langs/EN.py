1db0313cfc483ca018f106ed95a81bf1e19701ad
Pere Castagne <105001951+pere-castagne@users.noreply.github.com>
2023-09-14 21:08:12 +0200

    Add files via upload

---
+++ langs/EN.py
@@ -0,0 +1,66 @@
+###################################################################
+###### File for language : English
+###### For Opti Alim
+###### v 0.1.0
+###### by Pere Castagne
+###################################################################
+
+# Color theme
+lg_theme01 = 'Gray'
+lg_theme02 = 'White'
+lg_theme03 = 'Black'
+lg_theme04 = 'Azure'
+
+# Power Status
+lg_normal = 'Normal'
+lg_economy = 'Economy'
+lg_unknown = 'Unknown'
+lg_automatic = 'Automatic'
+lg_state = "Power status :"
+lg_show_mod = 'Which mode do you want to activate ?'
+
+# Adding an app/software
+lg_title_add = "Adding an app/software"
+lg_filetypes_exe = 'exe files'
+lg_filetypes_all = 'all files'
+lg_present_list = 'is already on the list.'
+lg_add_succ = 'has been added.'
+lg_add_failed = 'The addition was a failure...'
+lg_add_app = 'Add a program.'
+lg_import_app = 'Import'
+lg_show_list = 'Show list'
+
+# Mise à jour
+lg_maj_error = 'Update error'
+lg_maj_error2 = """A problem prevented Opti Alim from updating.
+Please try again later or update it manually."""
+lg_new_version1 = 'The new version'
+lg_new_version2 = '''is available
+
+Would you like to install it ?'''
+
+# Recurring text
+lg_back = 'Back'
+lg_refresh = 'Refresh'
+lg_delete = 'Delete'
+lg_save = 'Save'
+lg_no = 'No'
+lg_yes = 'Yes'
+lg_warning = 'Warning'
+
+# Launch error
+lg_lunch_error1 = 'Opti Alim Error'
+lg_lunch_error2 = '''Opti Alim stopped due to missing files!
+
+Repair is necessary.'''
+lg_missing_fileMa = '''Some files cannot be found, which may impact the operation of the software.
+
+We advise you to carry out the Opti Alim repair.'''
+lg_missing_filemi = '''Some files cannot be found.
+
+We advise you to carry out the Opti Alim repair.'''
+
+# Setting
+lg_setting = 'Setting'
+lg_setting_lang = 'Language :'
+lg_setting_theme = 'Theme :'
---
+++ langs/FR.py
@@ -0,0 +1,66 @@
+###################################################################
+###### Fichier pour la langue : Français
+###### Pour Opti Alim
+###### v 0.1.0
+###### by Pere Castagne
+###################################################################
+
+# Thème de couleur
+lg_theme01 = 'Gris'
+lg_theme02 = 'Blanc'
+lg_theme03 = 'Noir'
+lg_theme04 = 'Azure'
+
+# Etat de l'alimentation
+lg_normal = 'Normale'
+lg_economy = 'Economie'
+lg_unknown = 'Inconnu'
+lg_automatic = 'Automatique'
+lg_state = "Etat de l'alimentation :"
+lg_show_mod = 'Quel mode souhaitez-vous activer ?'
+
+# Ajout d'une application/logiciel
+lg_title_add = "Ajout d'une application/logiciel"
+lg_filetypes_exe = 'fichier exe'
+lg_filetypes_all = 'tous les fichiers'
+lg_present_list = 'est déjà présent dans la liste.'
+lg_add_succ = 'a été ajouté.'
+lg_add_failed = "L'ajout a été un échec..."
+lg_add_app = 'Ajouter un programme.'
+lg_import_app = 'Importer'
+lg_show_list = 'Afficher la liste'
+
+# Mise à jour
+lg_maj_error1 = 'Erreur de mise à jour'
+lg_maj_error2 = """Un problème a empêché la mise à jour d'Opti Alim.
+Veuillez ressayer plus tard ou de le mettre à jour manuellement."""
+lg_new_version1 = 'La nouvelle version'
+lg_new_version2 = '''est disponible
+
+Souhaitez-vous l'installer ?'''
+
+# Texte récurrent
+lg_back = 'Retour'
+lg_refresh = 'Actualiser'
+lg_delete = 'Supprimer'
+lg_save = 'Enregistrer'
+lg_no = 'Non'
+lg_yes = 'Oui'
+lg_warning = 'Avertissement'
+
+# Erreur de lancement
+lg_lunch_error1 = 'Erreur Opti Alim'
+lg_lunch_error2 = '''Arrêt d'Opti Alim suite à des fichiers manquants !
+
+Une réparation est nécessaire.'''
+lg_missing_fileMa = '''Certains fichiers sont introuvables, ce qui peut impacter le fonctionnement du logiciel.
+
+Nous vous conseillons d'effectuer la réparation d'Opti Alim.'''
+lg_missing_filemi = '''Certains fichiers sont introuvables.
+
+Nous vous conseillons d'effectuer la réparation d'Opti Alim.'''
+
+# Paramètre
+lg_setting = 'Paramètre'
+lg_setting_lang = 'Langue :'
+lg_setting_theme = 'Thème :'
